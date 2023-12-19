c8f0b15e4da6e74265bb3cd5625c3904079bdff8
Naomi Lagat <naomiejerutoe@gmail.com>
2023-12-19 11:14:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+
+function findMatching(arrayOfDrivers, string){
+  const findMatchingDriver = arrayOfDrivers.filter(driver => driver.toLowerCase() === string.toLowerCase());
+  return findMatchingDriver;
+}
+
+function fuzzyMatch(arrayOfDrivers, string){
+  const firstLetterMatch = arrayOfDrivers.filter(driver => driver.toLowerCase().startsWith(string.toLowerCase()));
+  return firstLetterMatch;
+}
+
+function matchName(arrayOfDriversObject, string){
+  const matchingName = arrayOfDriversObject.filter(driver => driver.name === string);
+  return matchingName;
+}
